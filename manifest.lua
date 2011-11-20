338f715f33f02cc52180468fd7b0ccd0c708d62f
Peter Drahoš <drahosp@gmail.com>
2011-11-20 16:47:22 +0100

    Updated manifest generator and manifest. Closing #61

--- manifest.lua
+++ manifest.lua
@@ -20,27 +20,27 @@ modules:close()
 -- Collect tags for each module
 local manifest = {}
 for name, url in pairs(repo) do
-    local remote = io.popen("git ls-remote --tags "..url.. " | tail -r")
+    local remote = io.popen("git ls-remote --tags "..url)
 	local tags = {}
-    for line in remote:lines() do
+	table.insert(tags, "0 refs/tags/master")
+  for line in remote:lines() do
 		table.insert(tags, line)
 	end
-	table.insert(tags, "0 refs/tags/master")
 
 	-- Generate manifest entry for each tag if it contains valid dist.info
-	for _,line in ipairs(tags) do
-        local hash, tag = line:match("([^%s]+)%srefs/tags/([^%s%^]+)$")
+	for line=#tags,1,-1 do 
+        local hash, tag = tags[line]:match("([^%s]+)%srefs/tags/([^%s%^]+)$")
         if hash and tag then
             -- Collect dist.info for each tag
             local url = "https://raw.github.com/LuaDist/"..name.."/"..tag.."/dist.info"
             local info = man.info(per.loadText(fet.get(url)) or "")
 			
-			-- Generate manifest entry for the dist
+			      -- Generate manifest entry for the dist
             if info then
-            	-- If master, mark dist version as scm
-				if tag == "master" then info.version = "scm" end
-			
-			    -- Small hack to generate correct filename
+                if (info.name ~= name) then print ("WARNINIG: Name mismatch in "..name.." and its dist.info") end
+            	  -- If master, mark dist version as scm
+				        if tag == "master" then info.version = "scm" end
+			          -- Small hack to generate correct filename
                 -- I apologize to GitHub for (ab)using their automated zip feature.
                 info.path = "https://nodeload.github.com/LuaDist/"..name.."/zipball/"..tag.."?/"..info.name.."-"..info.version..".dist"
                 print(info.name, info.version, info.path)
